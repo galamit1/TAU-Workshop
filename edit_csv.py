67857720adf2984f103f450cb04998464237bf0a
Gal Amit <agal@carbonblack.com>
2023-09-13 16:37:11 +0300

    arrange code

--- edit_csv.py
+++
@@ -1,5 +0,0 @@
-
-train_path = "train_labeled.csv"
-validation_path = "validation_labeled.csv"  # the validation set is the same as the training set, need to change this
-test_path = "test_labeled.csv"
-
